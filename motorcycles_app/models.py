69cd5c2f2b2e6b22535f5eac7afa18b2ab10b042
moisesfdasilva <msesfernandes@gmail.com>
2023-08-01 18:56:09 -0300

    feat: create models "Manufacturer" add "Motorcycle" in the motorcycles_app/models.py.

--- motorcycles_app/models.py
+++ motorcycles_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    founded = models.IntegerField
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def _str__(self):
+        return self.name
+
+
+class Motorcycle(models.Model):
+    model = models.CharField(max_length=100)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    year = models.IntegerField
+    engine_cc = models.IntegerField
+    type = models.CharField(max_length=30)
+    color = models.CharField(max_length=100)
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def _str__(self):
+        return f"{self.model} {self.manufacturer} {self.year} {self.engine_cc}"
