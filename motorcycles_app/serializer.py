bea17c36d36c97e83ae27d3037329766342700c9
moisesfdasilva <msesfernandes@gmail.com>
2023-08-01 20:26:09 -0300

    feat: create "ManufacturerSerializer" and "MotorcycleSerializer" in the motorcycles_app/serializer.py.

---
+++ motorcycles_app/serializer.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Manufacturer, Motorcycle
+
+
+class ManufacturerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Manufacturer
+        fields = '__all__'
+
+
+class MotorcycleSerializer(serializers.ModelSerializer):
+    manufacturer = ManufacturerSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Motorcycle
+        fields = '__all__'
