b649f3e432265b1a0b8ea7197b1e29678cdc9735
moisesfdasilva <msesfernandes@gmail.com>
2023-08-02 15:13:36 -0300

    feat: run the makemigrations and migration.

--- motorcycles_app/urls.py
+++ motorcycles_app/urls.py
@@ -3,6 +3,6 @@ from .apiviews import ManufacturerList, MotorcycleList
 
 
 urlpatterns = [
-    path("manufacturer/", ManufacturerList.as_view(), name="polls_list"),
-    path("motorcycle/", MotorcycleList.as_view(), name="polls_detail")
+    path("manufacturer/all", ManufacturerList.as_view(), name="polls_list"),
+    path("motorcycle/all", MotorcycleList.as_view(), name="polls_detail")
 ]
