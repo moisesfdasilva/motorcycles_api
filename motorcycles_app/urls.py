cf3c3366b647cddb0c518e542c4be371f4965397
moisesfdasilva <msesfernandes@gmail.com>
2023-08-03 20:56:15 -0300

    feat: create method GET, PUT, DELETE "motorcycle_by_pk" in the motorcycles_app/views.py.

--- motorcycles_app/urls.py
+++ motorcycles_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("manufacturer/new", views.new_manufacturer),
     path("motorcycle/new", views.new_motorcycle),
     path("manufacturer/<int:pk>", views.manufacturer_by_pk),
+    path("motorcycle/<int:pk>", views.motorcycle_by_pk),
 ]
--- motorcycles_app/views.py
+++ motorcycles_app/views.py
@@ -62,4 +62,34 @@ def manufacturer_by_pk(request, pk):
 
     if method == 'DELETE':
         manufacturer.delete()
-        return Response({"message": f"id {pk} removed"})
+        return Response({"message": f"Manufacturer id {pk} removed"})
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def motorcycle_by_pk(request, pk):
+    method = request.method
+
+    motorcycle = get_object_or_404(Motorcycle, pk=pk)
+
+    if method == 'GET':
+        data = MotorcycleSerializer(motorcycle).data
+        return Response(data)
+
+    if method == 'PUT':
+        serializer = MotorcycleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            motorcycle.manufacturer = Manufacturer.objects.get(
+                pk=request.data['manufacturer'])
+            motorcycle.model = request.data['model']
+            motorcycle.year = request.data['year']
+            motorcycle.engine_cc = request.data['engine_cc']
+            motorcycle.type = request.data['type']
+            motorcycle.color = request.data['color']
+            motorcycle.save()
+            data = MotorcycleSerializer(motorcycle).data
+            return Response(data)
+        return Response({"error": "bad request"}, status=404)
+
+    if method == 'DELETE':
+        motorcycle.delete()
+        return Response({"message": f"Motorcycle id {pk} removed"})
